43d6d559647fc7590ac7bec2241775c11a75f0ff
Nick Howes <n.howes@warwick.ac.uk>
2018-08-01 12:53:38 +0100

    Use Node 8 for build plan

--- bamboo-specs/src/main/java/uk/ac/warwick/casemanagement/CaseManagementSpec.java
+++ bamboo-specs/src/main/java/uk/ac/warwick/casemanagement/CaseManagementSpec.java
@@ -3,9 +3,16 @@ package uk.ac.warwick.casemanagement;
 import com.atlassian.bamboo.specs.api.BambooSpec;
 import com.atlassian.bamboo.specs.api.builders.deployment.Deployment;
 import com.atlassian.bamboo.specs.api.builders.notification.Notification;
+import com.atlassian.bamboo.specs.api.builders.plan.Job;
 import com.atlassian.bamboo.specs.api.builders.plan.Plan;
+import com.atlassian.bamboo.specs.api.builders.plan.Stage;
+import com.atlassian.bamboo.specs.api.builders.plan.artifact.Artifact;
 import com.atlassian.bamboo.specs.api.builders.project.Project;
+import com.atlassian.bamboo.specs.api.builders.requirement.Requirement;
 import com.atlassian.bamboo.specs.builders.notification.DeploymentStartedAndFinishedNotification;
+import com.atlassian.bamboo.specs.builders.task.*;
+import com.atlassian.bamboo.specs.model.task.ScriptTaskProperties;
+import com.atlassian.bamboo.specs.model.task.TestParserTaskProperties;
 import uk.ac.warwick.bamboo.specs.AbstractWarwickBuildSpec;
 
 import java.util.Collection;
@@ -37,7 +44,49 @@ public class CaseManagementSpec extends AbstractWarwickBuildSpec {
       build(PROJECT, "APP", "Case Management")
         .linkedRepository(LINKED_REPOSITORY)
         .description("Build application")
-        .activatorWithAssets("sbt")
+        .stage(
+          new Stage("Build stage")
+            .jobs(
+              new Job("Build and check", "BUILD")
+                .tasks(
+                  new VcsCheckoutTask()
+                    .description("Checkout source from default repository")
+                    .checkoutItems(new CheckoutItem().defaultRepository()),
+                  new NpmTask()
+                    .description("prune")
+                    .nodeExecutable("Node 8")
+                    .command("prune"),
+                  new NpmTask()
+                    .description("dependencies")
+                    .nodeExecutable("Node 8")
+                    .command("install -d"),
+                  new ScriptTask()
+                    .description("Run tests and package")
+                    .interpreter(ScriptTaskProperties.Interpreter.BINSH_OR_CMDEXE)
+                    .location(ScriptTaskProperties.Location.FILE)
+                    .fileFromPath("sbt")
+                    .argument("clean test:compile test universal:packageZipTarball")
+                    .environmentVariables("PATH=/usr/nodejs/8/bin")
+                )
+                .finalTasks(
+                  new TestParserTask(TestParserTaskProperties.TestType.JUNIT)
+                    .description("Parse test results")
+                    .resultDirectories("**/test-reports/*.xml"),
+                  TestParserTask.createMochaParserTask()
+                    .defaultResultDirectory()
+                )
+                .artifacts(
+                  new Artifact()
+                    .name("tar.gz")
+                    .copyPattern("app.tar.gz")
+                    .location("target/universal")
+                    .shared(true)
+                )
+                .requirements(
+                  new Requirement("Linux").matchValue("true").matchType(Requirement.MatchType.EQUALS)
+                )
+            )
+        )
         .slackNotifications(SLACK_CHANNEL, false)
         .build()
     );
