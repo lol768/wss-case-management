43b1b0af61bc8387164f7965ea51fd12fccedffc
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-08-02 11:35:23 +0100

    CASE-4 Allow users in the in-case-management-masqueraders WebGroup to masquerade as other users

--- app/controllers/RequestContext.scala
+++ app/controllers/RequestContext.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import play.api.mvc.RequestHeader
+import play.api.mvc.{Flash, RequestHeader}
 import services.Navigation
 import warwick.sso.{AuthenticatedRequest, SSOClient, User}
 
@@ -11,28 +11,32 @@ import scala.concurrent.{Await, Future}
 case class RequestContext(
   path: String,
   user: Option[User],
+  actualUser: Option[User],
   loginUrl: String,
   logoutUrl: String,
   navigation: Seq[Navigation],
+  flash: Flash,
   userAgent: Option[String],
   ipAddress: String
-)
+) {
+  def isMasquerading: Boolean = user != actualUser
+}
 
 object RequestContext {
 
-  def authenticated(sso: SSOClient, request: AuthenticatedRequest[_], navigation: Seq[Navigation]): RequestContext = RequestContext(sso, request, request.context.user, navigation)
+  def authenticated(sso: SSOClient, request: AuthenticatedRequest[_], navigation: Seq[Navigation]): RequestContext = RequestContext(sso, request, request.context.user, request.context.actualUser, navigation)
 
   def authenticated(sso: SSOClient, request: RequestHeader, navigation: Seq[Navigation]): RequestContext = {
     val eventualRequestContext = sso.withUser(request) { loginContext =>
-      Future.successful(Right(RequestContext(sso, request, loginContext.user, navigation)))
+      Future.successful(Right(RequestContext(sso, request, loginContext.user, loginContext.actualUser, navigation)))
     }.map(_.right.get)
 
     Await.result(eventualRequestContext, Duration.Inf)
   }
 
-  def anonymous(sso: SSOClient, request: RequestHeader, navigation: Seq[Navigation]): RequestContext = RequestContext(sso, request, None, navigation)
+  def anonymous(sso: SSOClient, request: RequestHeader, navigation: Seq[Navigation]): RequestContext = RequestContext(sso, request, None, None, navigation)
 
-  def apply(sso: SSOClient, request: RequestHeader, user: Option[User], navigation: Seq[Navigation]): RequestContext = {
+  def apply(sso: SSOClient, request: RequestHeader, user: Option[User], actualUser: Option[User], navigation: Seq[Navigation]): RequestContext = {
     val target = (if (request.secure) "https://" else "http://") + request.host + request.path
     val linkGenerator = sso.linkGenerator(request)
     linkGenerator.setTarget(target)
@@ -40,9 +44,11 @@ object RequestContext {
     RequestContext(
       path = request.path,
       user = user,
+      actualUser = actualUser,
       loginUrl = linkGenerator.getLoginUrl,
       logoutUrl = linkGenerator.getLogoutUrl,
       navigation = navigation,
+      flash = request.flash,
       userAgent = request.headers.get("User-Agent"),
       ipAddress = request.remoteAddress
     )
