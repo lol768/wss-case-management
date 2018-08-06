53972a17d0f431f7c4a79dd98ee232bd76b21001
Matthew Jones <matthew.jones@warwick.ac.uk>
2018-08-06 09:55:54 +0100

    CASE-23 - Audit log

--- app/services/RegistrationService.scala
+++ app/services/RegistrationService.scala
@@ -3,26 +3,38 @@ package services
 import com.google.inject.ImplementedBy
 import domain.Registrations
 import domain.dao.RegistrationDao
+import helpers.ServiceResults.ServiceResult
 import javax.inject.Inject
+import play.api.libs.json.Json
 import warwick.sso.UniversityID
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 @ImplementedBy(classOf[RegistrationServiceImpl])
 trait RegistrationService {
 
-  def save(studentSupportRegistration: Registrations.StudentSupport): Future[String]
+  def save(studentSupportRegistration: Registrations.StudentSupport)(implicit auditLogContext: AuditLogContext): Future[ServiceResult[String]]
 
   def getStudentSupport(universityID: UniversityID): Future[Option[Registrations.StudentSupport]]
 
 }
 
 class RegistrationServiceImpl @Inject()(
-  dao: RegistrationDao
-) extends RegistrationService {
+  dao: RegistrationDao,
+  auditService: AuditService
+)(implicit executionContext: ExecutionContext) extends RegistrationService {
 
-  override def save(studentSupportRegistration: Registrations.StudentSupport): Future[String] =
-    dao.save(studentSupportRegistration)
+  override def save(studentSupportRegistration: Registrations.StudentSupport)(implicit auditLogContext: AuditLogContext): Future[ServiceResult[String]] =
+    auditService.audit[String](
+      "SaveRegistration",
+      (id: String) => id,
+      "Registrations.StudentSupport",
+      Json.obj(
+        "universityId" -> studentSupportRegistration.universityID.string
+      )
+    ) {
+      dao.save(studentSupportRegistration).map(Right.apply)
+    }
 
   override def getStudentSupport(universityID: UniversityID): Future[Option[Registrations.StudentSupport]] =
     dao.getStudentSupport(universityID)
