2632cb34b04f6f13ab4af914b0e935c92596a44f
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-08-17 12:33:55 +0100

    Use execWithCommit

--- test/domain/CustomJdbcTypesTest.scala
+++ test/domain/CustomJdbcTypesTest.scala
@@ -39,7 +39,7 @@ class CustomJdbcTypesTest extends AbstractDaoTest {
 
       execWithCommit(table += entity) mustBe 1
 
-      val inserted = db.run(table.filter(_.id === entity.id).result.head).futureValue
+      val inserted = execWithCommit(table.filter(_.id === entity.id).result.head)
       inserted.id mustBe entity.id
       inserted.dt mustBe dt
 
@@ -47,6 +47,7 @@ class CustomJdbcTypesTest extends AbstractDaoTest {
       ts.toString mustBe "2018-08-17 09:44:43.182" // Converted 10:44am BST to UTC
 
       execWithCommit(table.filter(_.dt === entity.dt).result.headOption) mustBe 'defined
+      execWithCommit(table.schema.drop)
     }
 
     "work across DST boundaries" in new DatabaseFixture {
@@ -60,14 +61,15 @@ class CustomJdbcTypesTest extends AbstractDaoTest {
       val entity2 = Entity(UUID.randomUUID(), dt2)
       val entity3 = Entity(UUID.randomUUID(), dt3)
 
-      db.run(table += entity2).futureValue mustBe 1
-      db.run(table += entity3).futureValue mustBe 1
+      execWithCommit(table += entity2) mustBe 1
+      execWithCommit(table += entity3) mustBe 1
 
-      db.run(table.filter(_.id === entity2.id).map(_.dtButItsATimestamp).result.head).futureValue.toString mustBe "2018-10-28 00:30:00.0"
-      db.run(table.filter(_.id === entity3.id).map(_.dtButItsATimestamp).result.head).futureValue.toString mustBe "2018-10-28 01:30:00.0"
+      execWithCommit(table.filter(_.id === entity2.id).map(_.dtButItsATimestamp).result.head).toString mustBe "2018-10-28 00:30:00.0"
+      execWithCommit(table.filter(_.id === entity3.id).map(_.dtButItsATimestamp).result.head).toString mustBe "2018-10-28 01:30:00.0"
 
-      db.run(table.filter(_.id === entity2.id).result.head).futureValue mustBe entity2
-      db.run(table.filter(_.id === entity3.id).result.head).futureValue mustBe entity3
+      execWithCommit(table.filter(_.id === entity2.id).result.head) mustBe entity2
+      execWithCommit(table.filter(_.id === entity3.id).result.head) mustBe entity3
+      execWithCommit(table.schema.drop)
     }
   }
 
