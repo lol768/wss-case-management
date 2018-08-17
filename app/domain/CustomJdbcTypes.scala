52d0f232c1d49b15ebeb89f0a0139bf59a9a4abc
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-08-17 12:27:42 +0100

    CASE-30 Specify literal correctly

--- app/domain/CustomJdbcTypes.scala
+++ app/domain/CustomJdbcTypes.scala
@@ -1,18 +1,16 @@
 package domain
 
 import java.sql.{PreparedStatement, ResultSet, Timestamp}
-import java.time.{LocalDateTime, OffsetDateTime, ZoneId}
-import java.util.{Calendar, GregorianCalendar, SimpleTimeZone, TimeZone}
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDateTime, OffsetDateTime, ZoneId, ZoneOffset}
+import java.util.{Calendar, TimeZone}
 
 import enumeratum.SlickEnumSupport
 import helpers.JavaTime
 import play.api.libs.json.{JsValue, Json}
-import slick.ast.BaseTypedType
 import slick.jdbc.PostgresProfile.api._
 import slick.jdbc.{JdbcProfile, JdbcType, PostgresProfile}
 import warwick.sso.{GroupName, UniversityID, Usercode}
-
-import scala.reflect._
 
 object CustomJdbcTypes extends warwick.slick.jdbctypes.CustomJdbcTypes(PostgresProfile) with SlickEnumSupport {
   override val profile: JdbcProfile = PostgresProfile
@@ -43,39 +41,19 @@ object CustomJdbcTypes extends warwick.slick.jdbctypes.CustomJdbcTypes(PostgresP
     ts => LocalDateTime.ofInstant(ts.toInstant, ZoneId.systemDefault)
   )
 
-//  implicit val offsetDateTimeTypeMapper: JdbcType[OffsetDateTime] = new DriverJdbcType[OffsetDateTime]() {
-//    private[this] def referenceCalendar= Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-//
-//    private[this] def map(v: OffsetDateTime): Timestamp = Timestamp.from(v.toInstant)
-//    private[this] def comap(ts: Timestamp): OffsetDateTime = OffsetDateTime.ofInstant(ts.toInstant, JavaTime.timeZone)
-//
-//    def sqlType: Int = java.sql.Types.TIMESTAMP
-//    def setValue(v: OffsetDateTime, p: PreparedStatement, idx: Int): Unit = p.setTimestamp(idx, map(v), referenceCalendar)
-//    def getValue(r: ResultSet, idx: Int): OffsetDateTime = comap(r.getTimestamp(idx, referenceCalendar))
-//    def updateValue(v: OffsetDateTime, r: ResultSet, idx: Int): Unit = r.updateTimestamp(idx, map(v))
-//    override def valueToSQLLiteral(value: OffsetDateTime): String = timestampColumnType.valueToSQLLiteral(map(value))
-//  }
+  implicit val offsetDateTimeTypeMapper: JdbcType[OffsetDateTime] = new DriverJdbcType[OffsetDateTime]() {
+    private[this] val referenceCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    private[this] val literalDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
 
-  class UtcTimestampJdbcType extends DriverJdbcType[Timestamp] {
-    private[this] def referenceCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    private[this] def map(v: OffsetDateTime): Timestamp = Timestamp.from(v.toInstant)
+    private[this] def comap(ts: Timestamp): OffsetDateTime = OffsetDateTime.ofInstant(ts.toInstant, JavaTime.timeZone)
 
     def sqlType: Int = java.sql.Types.TIMESTAMP
-    def setValue(v: Timestamp, p: PreparedStatement, idx: Int): Unit = p.setTimestamp(idx, v, referenceCalendar)
-    def getValue(r: ResultSet, idx: Int): Timestamp = r.getTimestamp(idx, referenceCalendar)
-    def updateValue(v: Timestamp, r: ResultSet, idx: Int): Unit = r.updateTimestamp(idx, v)
-
-    // Force use of bind parameter rather than literal in WHERE clauses.
-    override def hasLiteralForm = false
-
-    /** Bug: Literal string is in JVM local time - should be same zone as [[referenceCalendar]] */
-    // override def valueToSQLLiteral(value: Timestamp): String = s"{ts '${formatLiteral(value)}'}"
-  }
-
-  val utcTimestampColumnType = new UtcTimestampJdbcType
-
-  implicit val offsetDateTimeTypeMapper: JdbcType[OffsetDateTime] = new MappedJdbcType[OffsetDateTime, Timestamp]()(utcTimestampColumnType, classTag[OffsetDateTime]) with BaseTypedType[OffsetDateTime] {
-    def map(dt: OffsetDateTime): Timestamp = Timestamp.from(dt.toInstant)
-    def comap(ts: Timestamp): OffsetDateTime = OffsetDateTime.ofInstant(ts.toInstant, JavaTime.timeZone)
+    def setValue(v: OffsetDateTime, p: PreparedStatement, idx: Int): Unit = p.setTimestamp(idx, map(v), referenceCalendar)
+    def getValue(r: ResultSet, idx: Int): OffsetDateTime = comap(r.getTimestamp(idx, referenceCalendar))
+    def updateValue(v: OffsetDateTime, r: ResultSet, idx: Int): Unit = r.updateTimestamp(idx, map(v))
+    override def valueToSQLLiteral(value: OffsetDateTime): String =
+      s"{ts '${value.atZoneSameInstant(ZoneOffset.UTC).format(literalDateTimeFormatter)}'}"
   }
 
   implicit val jsonTypeMapper: JdbcType[JsValue] = MappedColumnType.base[JsValue, String](Json.stringify, Json.parse)
--- test/domain/CustomJdbcTypesTest.scala
+++ test/domain/CustomJdbcTypesTest.scala
@@ -1,12 +1,12 @@
 package domain
 
-import java.sql.{PreparedStatement, Timestamp}
-import java.time.{OffsetDateTime, ZoneOffset, ZonedDateTime}
+import java.sql.Timestamp
+import java.time.{OffsetDateTime, ZonedDateTime}
 import java.util.UUID
 
-import slick.jdbc.PostgresProfile.api._
 import domain.dao.AbstractDaoTest
 import helpers.JavaTime
+import slick.jdbc.PostgresProfile.api._
 
 class CustomJdbcTypesTest extends AbstractDaoTest {
 
@@ -23,6 +23,7 @@ class CustomJdbcTypesTest extends AbstractDaoTest {
       def dtButItsATimestamp = column[Timestamp]("DT")
 
       def * = (id, dt).mapTo[Entity]
+      def pk = primaryKey("ENTITY_PK", id)
     }
 
     val table = TableQuery[EntityTable]
@@ -46,6 +47,27 @@ class CustomJdbcTypesTest extends AbstractDaoTest {
       ts.toString mustBe "2018-08-17 09:44:43.182" // Converted 10:44am BST to UTC
 
       execWithCommit(table.filter(_.dt === entity.dt).result.headOption) mustBe 'defined
+    }
+
+    "work across DST boundaries" in new DatabaseFixture {
+      val dt1 = ZonedDateTime.of(2018, 10, 28, 0, 30, 0, 0, JavaTime.timeZone)
+      val dt2 = dt1.plusHours(1).toOffsetDateTime
+      val dt3 = dt1.plusHours(2).toOffsetDateTime
+
+      dt2.toString mustBe "2018-10-28T01:30+01:00"
+      dt3.toString mustBe "2018-10-28T01:30Z"
+
+      val entity2 = Entity(UUID.randomUUID(), dt2)
+      val entity3 = Entity(UUID.randomUUID(), dt3)
+
+      db.run(table += entity2).futureValue mustBe 1
+      db.run(table += entity3).futureValue mustBe 1
+
+      db.run(table.filter(_.id === entity2.id).map(_.dtButItsATimestamp).result.head).futureValue.toString mustBe "2018-10-28 00:30:00.0"
+      db.run(table.filter(_.id === entity3.id).map(_.dtButItsATimestamp).result.head).futureValue.toString mustBe "2018-10-28 01:30:00.0"
+
+      db.run(table.filter(_.id === entity2.id).result.head).futureValue mustBe entity2
+      db.run(table.filter(_.id === entity3.id).result.head).futureValue mustBe entity3
     }
   }
 
