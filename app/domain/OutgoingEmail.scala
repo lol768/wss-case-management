944695744983e7b67a2bcb2d1d7fb32acdd21d6c
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-08-20 13:13:30 +0100

    CASE-24 Put requirements on OutgoingEmail() to avoid mistakes

--- app/domain/OutgoingEmail.scala
+++ app/domain/OutgoingEmail.scala
@@ -8,19 +8,21 @@ import play.api.libs.json._
 import play.api.libs.mailer.{Attachment, Email}
 import warwick.sso.Usercode
 
-import scala.language.higherKinds
-
 case class OutgoingEmail(
   id: Option[UUID] = None,
   created: OffsetDateTime = JavaTime.offsetDateTime,
   email: Email,
-  recipient: Option[Usercode],
+  recipient: Option[Usercode] = None,
   emailAddress: Option[String] = None,
   sent: Option[OffsetDateTime] = None,
   lastSendAttempt: Option[OffsetDateTime] = None,
   failureReason: Option[String] = None,
   updated: OffsetDateTime = JavaTime.offsetDateTime
-)
+) {
+  require(recipient.nonEmpty || emailAddress.nonEmpty, "One of recipient or emailAddress must be set")
+  require(email.attachments.isEmpty, "Storing emails directly with attachments is not yet supported")
+  require(email.to.isEmpty, "Don't use TO on the Email, set recipient or emailAddress")
+}
 
 object OutgoingEmail {
   // Attachments aren't attached to the persisted Email() but as a separate entity
