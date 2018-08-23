eb24debf7d0c28cfcbbae37354a9523ae96605a3
Nick Howes <n.howes@warwick.ac.uk>
2018-08-23 19:48:25 +0100

    CASE-62 Mark jquery as external.

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,9 +1,11 @@
-/* eslint-env browser, jquery */
+/* eslint-env browser */
+
+import $ from 'jquery';
 
 import 'core-js/modules/es6.object.assign';
 import FieldHistory from './field-history';
 
-jQuery(($) => {
+$(() => {
   $('[data-toggle="popover"]').popover();
 
   $('.field-history').each((i, container) => {
