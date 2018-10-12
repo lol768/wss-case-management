99c9b18d9025bdc8636dc6df1beaff0579d1aacf
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-10-12 16:55:37 +0100

    CASE-220 Table view of appointments

--- app/assets/js/appointment-calendar.js
+++ app/assets/js/appointment-calendar.js
@@ -5,6 +5,7 @@ import log from 'loglevel';
 import moment from 'moment-timezone';
 import 'fullcalendar';
 import { addQsToUrl, fetchWithCredentials } from './serverpipe';
+import { formatDateMoment, formatTimeMoment } from './dateFormats';
 
 const FC = $.fullCalendar; // a reference to FullCalendar's root namespace
 const {
@@ -12,6 +13,7 @@ const {
   EventPointing,
   EventRenderer,
   Scroller,
+  UnzonedRange,
   htmlEscape,
 } = FC; // the class that all views must inherit from
 
@@ -19,6 +21,7 @@ class TableView extends View {
   constructor(calendar, viewSpec) {
     super(calendar, viewSpec);
 
+    this.segSelector = '.fc-table-item';
     this.scroller = new Scroller({
       overflowX: 'hidden',
       overflowY: 'auto',
@@ -26,7 +29,7 @@ class TableView extends View {
   }
 
   renderSkeleton() {
-    this.el.addClass(`fc-list-view ${this.calendar.theme.getClass('listView')}`);
+    this.el.addClass('fc-table-view');
 
     this.scroller.render();
     this.scroller.el.appendTo(this.el);
@@ -49,15 +52,118 @@ class TableView extends View {
   }
 
   renderEmptyMessage() {
-    this.contentEl.html(
-      `<div class="fc-list-empty-wrap2">
-        <div class="fc-list-empty-wrap1">
-          <div class="fc-list-empty">
-            ${htmlEscape(this.opt('noEventsMessage'))}
-          </div>
-        </div>
-      </div>`,
-    );
+    this.contentEl.html(`<div class="fc-table-empty">${htmlEscape(this.opt('noEventsMessage'))}</div>`);
+  }
+
+  renderDates(dateProfile) {
+    const { calendar } = this;
+    const dayStart = calendar.msToUtcMoment(dateProfile.renderUnzonedRange.startMs, true);
+    const viewEnd = calendar.msToUtcMoment(dateProfile.renderUnzonedRange.endMs, true);
+    const dayDates = [];
+    const dayRanges = [];
+
+    while (dayStart < viewEnd) {
+      dayDates.push(dayStart.clone());
+
+      dayRanges.push(new UnzonedRange(
+        dayStart,
+        dayStart.clone().add(1, 'day'),
+      ));
+
+      dayStart.add(1, 'day');
+    }
+
+    this.dayDates = dayDates;
+    this.dayRanges = dayRanges;
+
+    // all real rendering happens in EventRenderer
+  }
+
+  componentFootprintToSegs(footprint) {
+    const { dayRanges } = this;
+    const segs = [];
+
+    for (let dayIndex = 0; dayIndex < dayRanges.length; dayIndex += 1) {
+      const segRange = footprint.unzonedRange.intersect(dayRanges[dayIndex]);
+
+      if (segRange) {
+        const seg = {
+          startMs: segRange.startMs,
+          endMs: segRange.endMs,
+          isStart: segRange.isStart,
+          isEnd: segRange.isEnd,
+          dayIndex,
+        };
+
+        segs.push(seg);
+
+        // detect when footprint won't go fully into the next day,
+        // and mutate the latest seg to the be the end.
+        if (
+          !seg.isEnd
+          && dayIndex + 1 < dayRanges.length
+          && footprint.unzonedRange.endMs < dayRanges[dayIndex + 1].startMs + this.nextDayThreshold
+        ) {
+          seg.endMs = footprint.unzonedRange.endMs;
+          seg.isEnd = true;
+          break;
+        }
+      }
+    }
+
+    return segs;
+  }
+
+  // render the event segments in the view
+  renderSegList(allSegs) {
+    const segsByDay = TableView.groupSegsByDay(allSegs); // sparse array
+    const tableEl = $('<table class="fc-table-table table table-default"><tbody/></table>');
+    const tbodyEl = tableEl.find('tbody');
+
+    for (let dayIndex = 0; dayIndex < segsByDay.length; dayIndex += 1) {
+      const daySegs = segsByDay[dayIndex];
+
+      if (daySegs) { // sparse array, so might be undefined
+        // append a day header
+        const { weekNumber } = daySegs[0].footprint.eventDef.miscProps;
+
+        tbodyEl.append(this.dayHeaderHtml(this.dayDates[dayIndex], weekNumber));
+
+        this.eventRenderer.sortEventSegs(daySegs);
+
+        for (let i = 0; i < daySegs.length; i += 1) {
+          tbodyEl.append(daySegs[i].el); // append event row
+        }
+      }
+    }
+
+    this.contentEl.empty().append(tableEl);
+  }
+
+  // Returns a sparse array of arrays, segs grouped by their dayIndex
+  static groupSegsByDay(segs) {
+    const segsByDay = []; // sparse array
+
+    for (let i = 0; i < segs.length; i += 1) {
+      const seg = segs[i];
+      (segsByDay[seg.dayIndex] || (segsByDay[seg.dayIndex] = []))
+        .push(seg);
+    }
+
+    return segsByDay;
+  }
+
+  // generates the HTML for the day headers that live amongst the event rows
+  dayHeaderHtml(dayDate, weekNumber) {
+    const weekHtml = (weekNumber !== this.lastWeekNumber) ? `<span class='fc-table-heading--week'>(week ${weekNumber})</span>` : '';
+    this.lastWeekNumber = weekNumber;
+
+    return `<tr class="fc-table-heading" data-date="${dayDate.format('YYYY-MM-DD')}">
+        <td class="${this.calendar.theme.getClass('widgetHeader')}" colspan="5">
+          ${this.buildGotoAnchorHtml(dayDate, { class: 'fc-table-heading--date' }, htmlEscape(formatDateMoment(dayDate)))}
+          ${weekHtml}
+        </td>
+      </tr>`;
   }
 }
 
@@ -72,104 +178,86 @@ class TableEventRenderer extends EventRenderer {
 
   // generates the HTML for a single event row
   fgSegHtml(seg) {
-    const { view } = this;
-    const { calendar } = view;
-    const { theme } = calendar;
     const eventFootprint = seg.footprint;
-    const { eventDef, componentFootprint } = eventFootprint;
+    const { eventDef } = eventFootprint;
     const { url } = eventDef;
-    const classes = ['fc-list-item'].concat(this.getClasses(eventDef));
-    const bgColor = this.getBgColor(eventDef);
-    let timeHtml;
-
-    if (componentFootprint.isAllDay) {
-      timeHtml = view.getAllDayHtml();
-    } else if (view.isMultiDayRange(componentFootprint.unzonedRange)) {
-      if (seg.isStart || seg.isEnd) { // outer segment that probably lasts part of the day
-        timeHtml = htmlEscape(this._getTimeText( // eslint-disable-line no-underscore-dangle
-          calendar.msToMoment(seg.startMs),
-          calendar.msToMoment(seg.endMs),
-          componentFootprint.isAllDay,
-        ));
-      } else { // inner segment that lasts the whole day
-        timeHtml = view.getAllDayHtml();
-      }
-    } else {
-      // Display the normal time text for the *event's* times
-      timeHtml = htmlEscape(this.getTimeText(eventFootprint));
-    }
+    const classes = ['fc-table-item'].concat(this.getClasses(eventDef));
 
     if (url) {
       classes.push('fc-has-url');
     }
 
+    const { start, end } = eventFootprint.eventInstance.dateProfile;
+    const {
+      clients,
+      key,
+      appointmentType,
+      teamMember,
+      location,
+      state,
+    } = eventDef.miscProps;
+
     return `<tr class="${classes.join(' ')}">
-      ${this.displayEventTime ? `<td class="fc-list-item-time ${theme.getClass('widgetContent')}">${timeHtml || ''}</td>` : ''}
-      <td class="fc-list-item-marker ${theme.getClass('widgetContent')}">
-        <span class="fc-event-dot" ${bgColor ? ` style="background-color:${bgColor}"` : ''}></span>
-      </td>
-      <td class="fc-list-item-title ${theme.getClass('widgetContent')}">
-      <a${url ? ` href="${htmlEscape(url)}"` : ''}>
-        ${htmlEscape(eventDef.title || '')}
-      </a>
-      </td>
+        <td class="fc-table-item--time col-sm-1">
+          <span class="fc-table-item--time--start-time">${formatTimeMoment(start)}</span>
+          <br />
+          <span class="fc-table-item--time--end-time">${formatTimeMoment(end)}</span>
+        </td>
+        <td class="fc-table-item--title col-sm-4">
+          <span class="fc-table-item--title--clients">
+            ${_.map(clients, client => `<a href="/team/client/${client.client.universityID}">${client.client.fullName}</a>`).join('<br />')}
+          </span>
+          <br />
+          <span class="fc-table-item--title--key">${key}</span>
+        </td>
+        <td class="fa-table-item--details col-sm-4">
+          <span class="fc-table-item--details--type">${appointmentType.description}</span>
+          <br />
+          <span class="fc-table-item--details--team-member">with ${teamMember.fullName}</span>
+        </td>
+        <td class="fa-table-item--location col-sm-2">
+          ${location ? htmlEscape(location.name) : ''}
+        </td>
+        <td class="fa-table-item--state col-sm-1">
+          ${state}
+        </td>
       </tr>`;
-  }
-
-
-  // like "4:00am"
-  computeEventTimeFormat() {
-    return this.opt('mediumTimeFormat');
   }
 }
 
 class TableEventPointing extends EventPointing {
+  // for events with a url, the whole <tr> should be clickable,
+  // but it's impossible to wrap with an <a> tag. simulate this.
+  handleClick(seg, ev) {
+    super.handleClick(seg, ev); // might prevent the default action
 
+    // not clicking on or within an <a> with an href
+    if (!$(ev.target).closest('a[href]').length) {
+      const { url } = seg.footprint.eventDef;
+
+      if (url && !ev.isDefaultPrevented()) { // jsEvent not cancelled in handler
+        window.location.href = url; // simulate link click
+      }
+    }
+  }
 }
 
 TableView.prototype.eventRendererClass = TableEventRenderer;
 TableView.prototype.eventPointingClass = TableEventPointing;
 
-// const TableView = View.extend({
-//   // called once when the view is instantiated, when the user switches to the view.
-//   // initialize member variables or do other setup tasks.
-//   initialize: () => log.error('Not implemented: initialize()'),
-//
-//   // responsible for displaying the skeleton of the view within the already-defined
-//   // this.el, a jQuery element.
-//   render: () => log.error('Not implemented: render()'),
-//
-//   // responsible for adjusting the pixel-height of the view. if isAuto is true, the
-//   // view may be its natural height, and `height` becomes merely a suggestion.
-//   setHeight: (height, isAuto) => log.error(`Not implemented: setHeight(${height}, ${isAuto})`),
-//
-//   // reponsible for rendering the given Event Objects
-//   renderEvents: events => log.error(`Not implemented: renderEvents(${events})`),
-//
-//   // responsible for undoing everything in renderEvents
-//   destroyEvents: log.error('Not implemented: destroyEvents()'),
-//
-//   // accepts a {start,end} object made of Moments, and must render the selection
-//   renderSelection: range => log.error(`Not implemented: renderSelection(${range})`),
-//
-//   // responsible for undoing everything in renderSelection
-//   destroySelection: () => log.error('Not implemented: destroySelection()'),
-// });
-
 FC.views.table = {
   class: TableView,
   buttonTextKey: 'table',
+  duration: { days: 7 },
   defaults: {
     buttonText: 'table',
-    tableDayFormat: 'LL',
     noEventsMessage: 'No appointments to display',
-    duration: { years: 1 },
   },
 };
 
 export default function AppointmentCalendar(container) {
-  const $container = $(container);
-  $container.fullCalendar({
+  const $calendar = $(container);
+  $calendar.fullCalendar({
     header: {
       left: 'title',
       center: 'agendaFourWeek,agendaWeek,agendaDay,table',
@@ -208,7 +296,6 @@ export default function AppointmentCalendar(container) {
         duration: { weeks: 4 },
         buttonText: 'next 4 weeks',
         columnFormat: 'ddd D/MM',
-        selectable: true,
       },
       agendaDay: {
         titleFormat: 'dddd, MMM D, YYYY',
@@ -218,7 +305,7 @@ export default function AppointmentCalendar(container) {
     },
     events: (start, end, timezone, callback) => {
       fetchWithCredentials(
-        addQsToUrl($container.data('events'), {
+        addQsToUrl($calendar.data('events'), {
           start: start.utc().toISOString(),
           end: end.utc().toISOString(),
           timezone,
@@ -232,6 +319,9 @@ export default function AppointmentCalendar(container) {
         .then((response) => {
           if (response.success) {
             callback(_.map(response.data, event => ({
+              ...event,
+
+              // FullCalendar props
               id: event.id,
               resourceIds: [event.teamMember.usercode, (event.location || {}).name],
               title: event.subject,
@@ -249,7 +339,7 @@ export default function AppointmentCalendar(container) {
     },
     select: (start, end) => {
       // Start creating an appointment at the defined start/end time
-      window.location = addQsToUrl($container.data('create'), {
+      window.location = addQsToUrl($calendar.data('create'), {
         start: start.utc().toISOString(),
         duration: moment.duration(end.diff(start)).asSeconds(),
       });
@@ -257,10 +347,10 @@ export default function AppointmentCalendar(container) {
   });
 
   // If I'm inside a tabbed container, force a re-render when I become visible
-  if ($container.closest('.tab-pane').length) {
+  if ($calendar.closest('.tab-pane').length) {
     $('a[data-toggle="tab"]').on('shown.bs.tab', () => {
-      if ($container.is(':visible')) {
-        $container.fullCalendar('render');
+      if ($calendar.is(':visible')) {
+        $calendar.fullCalendar('render');
       }
     });
   }
--- app/domain/Appointment.scala
+++ app/domain/Appointment.scala
@@ -9,6 +9,7 @@ import helpers.JavaTime
 import play.api.data.format.Formatter
 import play.api.data.{FormError, Forms, Mapping}
 import play.api.libs.json.{Json, Writes}
+import uk.ac.warwick.util.termdates.AcademicYear
 import warwick.sso.{User, Usercode}
 
 import scala.collection.immutable
@@ -88,6 +89,7 @@ object AppointmentRender {
     "key" -> o.appointment.key.string,
     "subject" -> o.appointment.subject(Some(userLookup), Some(o.clients)),
     "start" -> o.appointment.start,
+    "weekNumber" -> AcademicYear.forDate(o.appointment.start).getAcademicWeek(o.appointment.start).getWeekNumber,
     "end" -> o.appointment.end,
     "url" -> controllers.admin.routes.AppointmentController.view(o.appointment.key).url,
     "duration" -> o.appointment.duration,
@@ -97,7 +99,10 @@ object AppointmentRender {
       "usercode" -> o.appointment.teamMember.string,
       "fullName" -> userLookup.get(o.appointment.teamMember).flatMap(_.name.full),
     ),
-    "appointmentType" -> o.appointment.appointmentType,
+    "appointmentType" -> Json.obj(
+      "id" -> o.appointment.appointmentType,
+      "description" -> o.appointment.appointmentType.description,
+    ),
     "state" -> o.appointment.state,
     "cancellationReason" -> o.appointment.cancellationReason,
     "case" -> o.clientCase.map { clientCase =>
