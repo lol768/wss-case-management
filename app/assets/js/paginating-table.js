a1a7a651a48980bf5ec64a44e9646cae9f3f8e26
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-10-25 10:06:14 +0100

    CASE-299 Pagination tidy up

--- app/assets/js/paginating-table.js
+++ app/assets/js/paginating-table.js
@@ -6,13 +6,13 @@ export default function PaginatingTable(element) {
   $table.on('click', '.pagination-link', (e) => {
     const $pagination = $(e.target);
     const $existingPagination = $pagination.closest('tr');
-    $existingPagination.html('<i class="fas fa-spinner fa-pulse"></i> loading&hellip;');
+    $existingPagination.find('td').html('<i class="fas fa-spinner fa-pulse"></i> Loading&hellip;');
     $.get($pagination.attr('href'), (data, status, xhr) => {
       if (status === 'error') {
         $pagination.closest('td').text(`Unable to load content: ${xhr.statusText || xhr.status || 'error'}`);
       } else {
         const $newRows = $(data);
-        const $newPagination = $newRows.find('.pagination-row').detach();
+        const $newPagination = $newRows.filter('.pagination-row').detach();
         $table.find('tbody').append($newRows);
         $table.find('tfoot').append($newPagination);
         $existingPagination.remove();
