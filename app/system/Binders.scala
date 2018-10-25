58ac6a7312fb2da8c4de2dc3c1d9b3affa8dbc99
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-10-25 09:54:10 +0100

    CASE-298 Properly URL encode QueryStringBindables by deferring to bindableString

--- app/system/Binders.scala
+++ app/system/Binders.scala
@@ -7,43 +7,33 @@ import play.api.mvc.{PathBindable, QueryStringBindable}
 import warwick.sso.UniversityID
 
 object Binders {
-  implicit val universityIDPathBindable: PathBindable[UniversityID] = new PathBindable[UniversityID] {
-    override def bind(key: String, value: String): Either[String, UniversityID] =
-      Right(UniversityID(value))
+  implicit val universityIDPathBindable: PathBindable[UniversityID] =
+    PathBindable.bindableString.transform(
+      UniversityID.apply,
+      _.string
+    )
 
-    override def unbind(key: String, value: UniversityID): String =
-      value.string
-  }
+  implicit val universityIDQueryStringBindable: QueryStringBindable[UniversityID] =
+    QueryStringBindable.bindableString.transform(
+      UniversityID.apply,
+      _.string
+    )
 
-  implicit val universityIDQueryStringBindable: QueryStringBindable[UniversityID] = new QueryStringBindable[UniversityID] {
-    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, UniversityID]] =
-      params.get(key).flatMap(_.headOption).map { p => Right(UniversityID(p)) }
+  implicit val offsetDateTimeQueryStringBindable: QueryStringBindable[OffsetDateTime] =
+    QueryStringBindable.bindableString.transform(
+      OffsetDateTime.parse(_, JavaTime.iSO8601DateFormat),
+      _.format(JavaTime.iSO8601DateFormat)
+    )
 
-    override def unbind(key: String, value: UniversityID): String =
-      s"$key=${value.string}"
-  }
+  implicit val zoneIdQueryStringBindable: QueryStringBindable[ZoneId] =
+    QueryStringBindable.bindableString.transform(
+      ZoneId.of,
+      _.getId
+    )
 
-  implicit val offsetDateTimeQueryStringBindable: QueryStringBindable[OffsetDateTime] = new QueryStringBindable[OffsetDateTime] {
-    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, OffsetDateTime]] =
-      params.get(key).flatMap(_.headOption).map { p => Right(OffsetDateTime.parse(p, JavaTime.iSO8601DateFormat)) }
-
-    override def unbind(key: String, value: OffsetDateTime): String =
-      s"$key=${value.format(JavaTime.iSO8601DateFormat)}"
-  }
-
-  implicit val zoneIdQueryStringBindable: QueryStringBindable[ZoneId] = new QueryStringBindable[ZoneId] {
-    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, ZoneId]] =
-      params.get(key).flatMap(_.headOption).map { p => Right(ZoneId.of(p)) }
-
-    override def unbind(key: String, value: ZoneId): String =
-      s"$key=${value.getId}"
-  }
-
-  implicit val durationQueryStringBindable: QueryStringBindable[Duration] = new QueryStringBindable[Duration] {
-    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Duration]] =
-      params.get(key).flatMap(_.headOption).map { p => Right(Duration.ofSeconds(p.toLong)) }
-
-    override def unbind(key: String, value: Duration): String =
-      s"$key=${value.getSeconds}"
-  }
+  implicit val durationQueryStringBindable: QueryStringBindable[Duration] =
+    QueryStringBindable.bindableLong.transform(
+      Duration.ofSeconds,
+      _.getSeconds
+    )
 }
