7d33bfc4e91d14c73065b987a3f70db15fcaf66d
Matthew Jones <matthew.jones@warwick.ac.uk>
2018-11-05 14:35:49 +0000

    CASE-333 - Fix merge

--- conf/evolutions/default/56.sql
+++ conf/evolutions/default/56.sql
@@ -58,11 +58,3 @@ DROP TABLE DSA_FUNDING_TYPE;
 
 DROP TABLE DSA_APPLICATION_VERSION;
 DROP TABLE DSA_APPLICATION;
-
-# --- !Ups
-alter table appointment add outcome VARCHAR(100);
-alter table appointment_version add outcome VARCHAR(100);
-
-# --- !Downs
-alter table appointment drop column outcome;
-alter table appointment_version drop column outcome;
