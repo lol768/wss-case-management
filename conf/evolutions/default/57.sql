8df164a3d4c321d19eceea62d8fed2e5ab636125
Matthew Jones <matthew.jones@warwick.ac.uk>
2018-11-06 15:07:45 +0000

    CASE-334 - Fix merge

--- conf/evolutions/default/57.sql
+++ conf/evolutions/default/57.sql
@@ -5,11 +5,3 @@ alter table appointment_version add outcome VARCHAR(100);
 # --- !Downs
 alter table appointment drop column outcome;
 alter table appointment_version drop column outcome;
-
-# --- !Ups
-ALTER TABLE USER_REGISTRATION ADD COLUMN LAST_INVITED_UTC timestamp(3) DEFAULT current_date NOT NULL;
-ALTER TABLE USER_REGISTRATION_VERSION ADD COLUMN LAST_INVITED_UTC timestamp(3) DEFAULT current_date NOT NULL;
-
-# --- !Downs
-ALTER TABLE USER_REGISTRATION DROP COLUMN LAST_INVITED_UTC;
-ALTER TABLE USER_REGISTRATION_VERSION DROP COLUMN LAST_INVITED_UTC;
