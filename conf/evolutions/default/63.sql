b18602d6b3faa34f7c891037502d88ec4d242e0a
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-11-09 15:17:52 +0000

    Smarter evo

--- conf/evolutions/default/63.sql
+++ conf/evolutions/default/63.sql
@@ -1,23 +1,24 @@
 # --- !Ups
 alter table APPOINTMENT
-  alter outcome type varchar(100)[] using array[outcome],
+  alter outcome type varchar(100)[] using case when outcome is null then '{}' else array[outcome] end,
   alter outcome set default '{}',
   alter outcome set not null;
 alter table APPOINTMENT_VERSION
-  alter outcome type varchar(100)[] using array[outcome],
+  alter outcome type varchar(100)[] using case when outcome is null then '{}' else array[outcome] end,
   alter outcome set default '{}',
   alter outcome set not null;
-
--- junk existing data
-update appointment set outcome = '{}';
-update appointment_version set outcome = '{}';
 
 # --- !Downs
 alter table APPOINTMENT
   alter outcome drop not null,
-  alter outcome drop default,
-  alter outcome type varchar(100);
+  alter outcome drop default;
+
+update appointment set outcome = null;
+alter table appointment alter outcome type varchar(100);
+
 alter table APPOINTMENT_VERSION
   alter outcome drop not null,
-  alter outcome drop default,
-  alter outcome type varchar(100);
+  alter outcome drop default;
+
+update appointment_version set outcome = null;
+alter table appointment_version alter outcome type varchar(100);
