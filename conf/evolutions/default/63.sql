ead643e084ae01b04ad61d72160e333e95b5a5b2
Matthew Jones <matthew.jones@warwick.ac.uk>
2018-11-09 16:54:02 +0000

    CASE-364 - Fix merge

--- conf/evolutions/default/63.sql
+++ conf/evolutions/default/63.sql
@@ -22,16 +22,3 @@ alter table APPOINTMENT_VERSION
 
 update appointment_version set outcome = null;
 alter table appointment_version alter outcome type varchar(100);
-
-# --- !Ups
-alter table client_case add student_support_issue_types VARCHAR(100)[] default '{}' not null;
-alter table client_case add student_support_issue_type_other text null;
-alter table client_case_version add student_support_issue_types VARCHAR(100)[] default '{}' not null;
-alter table client_case_version add student_support_issue_type_other text null;
-
-# --- !Downs
-alter table client_case drop student_support_issue_types;
-alter table client_case_version drop student_support_issue_types;
-alter table client_case drop student_support_issue_type_other;
-alter table client_case_version drop student_support_issue_type_other;
-
