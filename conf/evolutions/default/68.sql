96b3aaa0587d7d282ba3ba847a087c7e71ffc0aa
Matthew Jones <matthew.jones@warwick.ac.uk>
2018-11-13 15:46:32 +0000

    CASE-363 - Fix merge

--- conf/evolutions/default/68.sql
+++ conf/evolutions/default/68.sql
@@ -7,11 +7,3 @@ ALTER TABLE CLIENT_SUMMARY ADD COLUMN MENTAL_HEALTH_RISK BOOL NULL;
 ALTER TABLE CLIENT_SUMMARY_VERSION ADD COLUMN MENTAL_HEALTH_RISK BOOL NULL;
 
 CREATE INDEX IDX_CLIENT_SUMMARY_MHR ON CLIENT_SUMMARY (MENTAL_HEALTH_RISK);
-# --- !Ups
-alter table client_case add severity_of_problem VARCHAR(100) null;
-alter table client_case_version add severity_of_problem VARCHAR(100) null;
-
-# --- !Downs
-alter table client_case drop severity_of_problem;
-alter table client_case_version drop severity_of_problem;
-
---
+++ conf/evolutions/default/70.sql
@@ -0,0 +1,7 @@
+# --- !Ups
+alter table client_case add severity_of_problem VARCHAR(100) null;
+alter table client_case_version add severity_of_problem VARCHAR(100) null;
+
+# --- !Downs
+alter table client_case drop severity_of_problem;
+alter table client_case_version drop severity_of_problem;
