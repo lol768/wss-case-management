ff302b0daf444e657793838aac8f06c8c6f9bbf7
Nick Howes <n.howes@warwick.ac.uk>
2018-12-02 21:06:40 +0000

    CASE-405 Quick filters for notes and docs.
    I don't much like the focus and hover styles for buttons as they look way too much like the active style, which makes it hard to tell visually which radios are active.

---
+++ app/assets/js/quick-filter.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+
+export default function QuickFilter(container) {
+  const $container = $(container);
+  const targetClassPrefix = $container.data('target-class-prefix');
+  const $targetContainer = $($container.data('target'));
+  const $items = $targetContainer.children();
+  const $checkboxes = $container.find('input:checkbox');
+
+  $checkboxes.on('change', () => {
+    const types = $checkboxes.filter(':checked').map((i, el) => el.value);
+    const showAll = types.length === 0 || types.length >= $checkboxes.length;
+
+    if (showAll) {
+      $items.show();
+    } else {
+      $items.hide();
+      types.each((i, t) => {
+        const selector = `.${targetClassPrefix}${t}`;
+        $items.filter(selector).show();
+      });
+    }
+  });
+}
