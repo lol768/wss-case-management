a17126e57271940def4aa62a24ea5f2471250f1a
Nick Howes <n.howes@warwick.ac.uk>
2019-01-16 19:45:07 +0000

    CASE-448 Don't compile everything in node_modules.
    Just anything under @universityofwarwick.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = function(api) {
-  api.cache(true);
+  // Cache results until NODE_ENV changes
+  api.cache.using(() => process.env.NODE_ENV);
 
   return {
     "presets": ["@babel/preset-env"],
